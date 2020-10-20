2d531e1b3c23873e772037216f4e8e265d4c6c3d
Sérgio Vilar <sergiovilar.r@gmail.com>
2020-10-20 00:15:50 -0300

    Adicionados mais pontos de alagamento

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,32 @@
-return if Location.all.size > 0
+locations = [
+  {
+    title: 'Principal dos Bancários',
+    address: 'R. Bancários Sérgio Guerra',
+    location: '-7.15016224,-34.8407793'
+  },
+  {
+    title: 'CBTU',
+    address: 'Avenida Sanhauá',
+    location: '-7.11494057,-34.89027679'
+  },
+  {
+    title: 'Entrada do Geisel',
+    address: 'Avenida Valdemar Naziazeno',
+    location: '-7.17158561,-34.87421304'
+  },
+  {
+    title: 'Posto 99 x Ecomedical',
+    address: 'Rua Antônio Rabelo Júnior',
+    location: '-7.11897015,-34.83705908'
+  },
+]
 
-location = Location.create({
-  title: 'Principal dos Bancários',
-  address: 'R. Bancários Sérgio Guerra x R. João Batista Maia',
-  location: '-7.15016224,-34.8407793'
-})
+return if Location.all.size == locations.size
 
-Status.create({
-  location_id: location.id,
-  level: 0
-})
+locations.map do |loc|
+  location = Location.create(loc)
+  Status.create({
+    location_id: location.id,
+    level: 0
+  })
+end
